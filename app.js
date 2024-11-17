54d433d43774ef1178f13dcff30eaac0ebf712be
IsaacDeMelo <isaacangelo2005@gmail.com>
2024-11-17 16:58:43 -0300

    updateflash

--- app.js
+++ app.js
@@ -4,12 +4,67 @@ const dotenv = require('dotenv');
 const Message = require('./models/message.js');
 const User = require('./models/user.js');
 const moment = require('moment-timezone');
+const http = require('http');
+const { Server } = require('socket.io');
+
 let CurrentUser = 0;
 
 dotenv.config();
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app); // Cria o servidor HTTP
+const io = new Server(server); // Adiciona o WebSocket ao servidor
+// Conexão WebSocket
+io.on('connection', async (socket) => {
+    console.log('Novo cliente conectado');
+
+    try {
+        // Envia mensagens já existentes ao cliente
+        const messages = await Message.find();
+        socket.emit('updateMessages', messages);
+    } catch (err) {
+        console.error('Erro ao buscar mensagens ao conectar:', err);
+    }
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+
+const notifyClients = () => {
+    Message.find()
+        .then((messages) => {
+            io.emit('updateMessages', messages);
+        })
+        .catch((err) => console.error('Erro ao buscar mensagens:', err));
+};
+
+app.post('/api/messages', async (req, res) => {
+    const { usuario, texto } = req.body;
+    const username = usuario;
+    const user = await User.findOne({ username });
+    const foto = user.perfil;
+    const created = moment()
+        .tz('America/Sao_Paulo')
+        .format('DD [de] MMMM [de] YYYY, HH:mm:ss');
+
+    try {
+        const newMessage = new Message({ usuario, texto, foto, created });
+        await newMessage.save();
+
+        notifyClients(); // Atualiza os clientes
+        res.status(201).json(newMessage);
+    } catch (err) {
+        console.error('Erro ao salvar mensagem:', err.message);
+        res.status(500).json({ error: 'Erro ao salvar mensagem' });
+    }
+});
+
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true })); // Para processar dados de formulários
@@ -123,7 +178,7 @@ app.get('/api/messages', async (req, res) => {
 });
 
 // Inicia o servidor e conecta ao MongoDB
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
     connectDB();
 });
